e3fd9c57cb335371c4021b50b762c83041558d27
Elliott Mantock <emantock@gmail.com>
2016-02-10 15:21:00 -0500

    wrote some tuple methods

--- Heap/src/heap/Tuple.java
+++ Heap/src/heap/Tuple.java
@@ -1,25 +1,28 @@
 package heap;
 
+import java.util.Arrays;
+import global.GlobalConst;
+
 /**
  * Created by david on 2/5/16.
  */
-public class Tuple {
+public class Tuple implements GlobalConst {
   private int length;
   private byte[] tupleByteArray;
 
-  public Tuple(byte[] bytes, int i, int i1) {
-
+  public Tuple(byte[] byteArray, int from, int to) {
+    tupleByteArray = Arrays.copyOfRange(byteArray, from, to);
   }
 
   public Tuple() {
-
+    tupleByteArray = new byte[MAX_TUPSIZE];
   }
 
   public int getLength() {
-      return length;
+    return tupleByteArray.length;
   }
 
   public byte[] getTupleByteArray() {
-      return tupleByteArray;
+    return tupleByteArray;
   }
 }
