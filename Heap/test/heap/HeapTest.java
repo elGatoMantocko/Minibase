d7dad1d4dc25752fdc511efa4b1fcbf697a5942c
Elliott Mantock <emantock@gmail.com>
2016-02-10 12:24:47 -0500

    use similar makefile from bufmgr

---
+++ Heap/test/heap/HeapTest.java
@@ -0,0 +1,27 @@
+package heap;
+
+import global.Page;
+import global.PageId;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by david on 2/9/16.
+ */
+public class HeapTest {
+
+    HeapFile heap;
+
+    @Before @Ignore
+    public void setUp() throws Exception {
+
+    }
+
+    @Test @Ignore
+    public void testGetNumBuffers() throws Exception {
+
+    }
+}
